ffd33f5aa81c9ecf4219087d99c9610745f41f28
Adnurhytia <adindanurhayati30@gmail.com>
2023-07-07 23:24:14 +0700

    create btnDelete and use method refreshform

--- UAS_Kelompok21_PABD/Data Peminjam.cs
+++ UAS_Kelompok21_PABD/Data Peminjam.cs
@@ -59,7 +59,7 @@ namespace UAS_Kelompok21_PABD
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            
+            refreshform();
         }
 
         private void btnSave_Click(object sender, EventArgs e)

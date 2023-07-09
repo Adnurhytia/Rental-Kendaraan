5438243eb4f8e983a8795856943928fa33cec20f
Adnurhytia <adindanurhayati30@gmail.com>
2023-07-09 22:51:56 +0700

    Membuat CRUD pada Form Transaksi

--- UAS_Kelompok21_PABD/Data Transaksi.Designer.cs
+++ UAS_Kelompok21_PABD/Data Transaksi.Designer.cs
@@ -37,12 +37,17 @@
             this.label6 = new System.Windows.Forms.Label();
             this.tbxIdTransaksi = new System.Windows.Forms.TextBox();
             this.tbxTotalHarga = new System.Windows.Forms.TextBox();
-            this.cbcMetode = new System.Windows.Forms.ComboBox();
-            this.tbxPlatNmr = new System.Windows.Forms.TextBox();
-            this.tbxIdPeminjam = new System.Windows.Forms.TextBox();
+            this.cbxMetode = new System.Windows.Forms.ComboBox();
             this.txbDenda = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cbxIDPeminjam = new System.Windows.Forms.ComboBox();
+            this.cbxPlatNmr = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbxDelete = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,18 +55,18 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(537, 79);
+            this.dataGridView1.Location = new System.Drawing.Point(555, 64);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(559, 309);
+            this.dataGridView1.Size = new System.Drawing.Size(736, 309);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 106);
+            this.label1.Location = new System.Drawing.Point(35, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(148, 31);
             this.label1.TabIndex = 1;
@@ -71,7 +76,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 151);
+            this.label2.Location = new System.Drawing.Point(35, 121);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(147, 31);
             this.label2.TabIndex = 2;
@@ -81,7 +86,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(29, 196);
+            this.label3.Location = new System.Drawing.Point(35, 166);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(253, 31);
             this.label3.TabIndex = 3;
@@ -91,7 +96,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(29, 288);
+            this.label4.Location = new System.Drawing.Point(35, 258);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(89, 31);
             this.label4.TabIndex = 4;
@@ -101,7 +106,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(30, 240);
+            this.label5.Location = new System.Drawing.Point(36, 210);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(156, 31);
             this.label5.TabIndex = 5;
@@ -111,7 +116,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(31, 339);
+            this.label6.Location = new System.Drawing.Point(37, 309);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(115, 31);
             this.label6.TabIndex = 6;
@@ -119,77 +124,136 @@
             // 
             // tbxIdTransaksi
             // 
-            this.tbxIdTransaksi.Location = new System.Drawing.Point(343, 112);
+            this.tbxIdTransaksi.Location = new System.Drawing.Point(349, 82);
             this.tbxIdTransaksi.Name = "tbxIdTransaksi";
             this.tbxIdTransaksi.Size = new System.Drawing.Size(156, 26);
             this.tbxIdTransaksi.TabIndex = 7;
             // 
             // tbxTotalHarga
             // 
-            this.tbxTotalHarga.Location = new System.Drawing.Point(343, 157);
+            this.tbxTotalHarga.Location = new System.Drawing.Point(349, 127);
             this.tbxTotalHarga.Name = "tbxTotalHarga";
             this.tbxTotalHarga.Size = new System.Drawing.Size(156, 26);
             this.tbxTotalHarga.TabIndex = 8;
             // 
-            // cbcMetode
+            // cbxMetode
             // 
-            this.cbcMetode.FormattingEnabled = true;
-            this.cbcMetode.Location = new System.Drawing.Point(343, 202);
-            this.cbcMetode.Name = "cbcMetode";
-            this.cbcMetode.Size = new System.Drawing.Size(156, 28);
-            this.cbcMetode.TabIndex = 9;
-            // 
-            // tbxPlatNmr
-            // 
-            this.tbxPlatNmr.Location = new System.Drawing.Point(343, 345);
-            this.tbxPlatNmr.Name = "tbxPlatNmr";
-            this.tbxPlatNmr.Size = new System.Drawing.Size(156, 26);
-            this.tbxPlatNmr.TabIndex = 10;
-            // 
-            // tbxIdPeminjam
-            // 
-            this.tbxIdPeminjam.Location = new System.Drawing.Point(343, 246);
-            this.tbxIdPeminjam.Name = "tbxIdPeminjam";
-            this.tbxIdPeminjam.Size = new System.Drawing.Size(156, 26);
-            this.tbxIdPeminjam.TabIndex = 11;
+            this.cbxMetode.FormattingEnabled = true;
+            this.cbxMetode.Items.AddRange(new object[] {
+            "Cash",
+            "Debit",
+            "E-money"});
+            this.cbxMetode.Location = new System.Drawing.Point(349, 172);
+            this.cbxMetode.Name = "cbxMetode";
+            this.cbxMetode.Size = new System.Drawing.Size(156, 28);
+            this.cbxMetode.TabIndex = 9;
             // 
             // txbDenda
             // 
-            this.txbDenda.Location = new System.Drawing.Point(343, 294);
+            this.txbDenda.Location = new System.Drawing.Point(349, 264);
             this.txbDenda.Name = "txbDenda";
             this.txbDenda.Size = new System.Drawing.Size(156, 26);
             this.txbDenda.TabIndex = 12;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(343, 391);
+            this.btnAdd.Location = new System.Drawing.Point(349, 361);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(64, 30);
+            this.btnAdd.Size = new System.Drawing.Size(75, 40);
             this.btnAdd.TabIndex = 13;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(343, 437);
+            this.btnUpdate.Location = new System.Drawing.Point(430, 361);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 32);
+            this.btnUpdate.Size = new System.Drawing.Size(75, 40);
             this.btnUpdate.TabIndex = 14;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(349, 410);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 40);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(430, 410);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 40);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // cbxIDPeminjam
+            // 
+            this.cbxIDPeminjam.FormattingEnabled = true;
+            this.cbxIDPeminjam.Location = new System.Drawing.Point(349, 216);
+            this.cbxIDPeminjam.Name = "cbxIDPeminjam";
+            this.cbxIDPeminjam.Size = new System.Drawing.Size(156, 28);
+            this.cbxIDPeminjam.TabIndex = 17;
+            // 
+            // cbxPlatNmr
+            // 
+            this.cbxPlatNmr.FormattingEnabled = true;
+            this.cbxPlatNmr.Location = new System.Drawing.Point(349, 309);
+            this.cbxPlatNmr.Name = "cbxPlatNmr";
+            this.cbxPlatNmr.Size = new System.Drawing.Size(156, 28);
+            this.cbxPlatNmr.TabIndex = 18;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(555, 379);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(80, 31);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(552, 420);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 20);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "deletebyId";
+            // 
+            // tbxDelete
+            // 
+            this.tbxDelete.Location = new System.Drawing.Point(674, 417);
+            this.tbxDelete.Name = "tbxDelete";
+            this.tbxDelete.Size = new System.Drawing.Size(125, 26);
+            this.tbxDelete.TabIndex = 21;
             // 
             // Data_Transaksi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1123, 492);
+            this.ClientSize = new System.Drawing.Size(1317, 492);
+            this.Controls.Add(this.tbxDelete);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cbxPlatNmr);
+            this.Controls.Add(this.cbxIDPeminjam);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txbDenda);
-            this.Controls.Add(this.tbxIdPeminjam);
-            this.Controls.Add(this.tbxPlatNmr);
-            this.Controls.Add(this.cbcMetode);
+            this.Controls.Add(this.cbxMetode);
             this.Controls.Add(this.tbxTotalHarga);
             this.Controls.Add(this.tbxIdTransaksi);
             this.Controls.Add(this.label6);
@@ -219,11 +283,16 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox tbxIdTransaksi;
         private System.Windows.Forms.TextBox tbxTotalHarga;
-        private System.Windows.Forms.ComboBox cbcMetode;
-        private System.Windows.Forms.TextBox tbxPlatNmr;
-        private System.Windows.Forms.TextBox tbxIdPeminjam;
+        private System.Windows.Forms.ComboBox cbxMetode;
         private System.Windows.Forms.TextBox txbDenda;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ComboBox cbxIDPeminjam;
+        private System.Windows.Forms.ComboBox cbxPlatNmr;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbxDelete;
     }
 }
--- UAS_Kelompok21_PABD/Data Transaksi.cs
+++ UAS_Kelompok21_PABD/Data Transaksi.cs
@@ -2,24 +2,196 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using System.Windows.Input;
 
 namespace UAS_Kelompok21_PABD
 {
     public partial class Data_Transaksi : Form
     {
+        private string stringConnection = "Data Source=DESKTOP-4IT269M\\ADINDANURHAYATI;Initial Catalog=Peminjaman_Kendaraan;User ID=sa;Password=3007dinda";
+        private SqlConnection koneksi;
+        private SqlCommand command;
+        private SqlDataAdapter adapter;
         public Data_Transaksi()
         {
             InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            refreshform();
+            dataGridView();
+            LoadPeminjamData();
         }
 
         private void Data_Transaksi_Load(object sender, EventArgs e)
         {
 
+        }
+        private void refreshform()
+        {
+            tbxIdTransaksi.Text = "";
+            tbxTotalHarga.Text = "";
+            cbxMetode.Text = "";
+            cbxIDPeminjam.Text = "";
+            txbDenda.Text = "";
+            cbxPlatNmr.Text = "";
+            tbxIdTransaksi.Enabled = false;
+            btnSave.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            tbxIdTransaksi.Enabled = true;
+            btnSave.Enabled = true;
+            btnDelete.Enabled = true;
+        }
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select * from dbo.Transaksi";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string str = "DELETE FROM Transaksi WHERE id_transaksi = @id_transaksi";
+
+            using (SqlConnection conn = new SqlConnection(stringConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand(str, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id_peminjam", tbxDelete.Text);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data Berhasil Dihapus");
+                        dataGridView();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message + " (Error Code: " + ex.Number + ")");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string idTransaksi = tbxIdTransaksi.Text;
+            string tHarga = tbxTotalHarga.Text;
+            string mPembayaran = cbxMetode.Text;
+            string idPeminjam = cbxIDPeminjam.Text;
+            string denda = txbDenda.Text;
+            string pltNmr = cbxPlatNmr.Text;
+
+            if (idTransaksi == "")
+            {
+                MessageBox.Show("Masukkan ID Transaksi", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "insert into dbo.Transaksi (id_transaksi,total_harga,metode_pembayaran,denda,id_peminjam,plat_nmr)" + "values(@id_transaksi,@total_harga,@metode_pembayaran,@denda,@id_peminjam,plat_nmr)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("id_transaksi", idTransaksi));
+                cmd.Parameters.Add(new SqlParameter("total_harga", tHarga));
+                cmd.Parameters.Add(new SqlParameter("metode_pembayaran", mPembayaran));
+                cmd.Parameters.Add(new SqlParameter("id_peminjam", idPeminjam));
+                cmd.Parameters.Add(new SqlParameter("denda", denda));
+                cmd.Parameters.Add(new SqlParameter("plat_nmr", pltNmr));
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string str = "UPDATE Transaksi SET id_transaksi = @id_transaksi,total_harga=  @total_harga,metode_pembayaran= @metode_pembayaran,denda= @denda,id_peminjam= @id_peminjam,plat_nmr= @plat_nmr WHERE id_transaksi = @id_transaksi";
+
+            using (SqlConnection conn = new SqlConnection(stringConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand(str, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id_transaksi", tbxIdTransaksi.Text);
+                    cmd.Parameters.AddWithValue("@total_harga", tbxTotalHarga.Text);
+                    cmd.Parameters.AddWithValue("@metode_pembayaran", cbxMetode.Text);
+                    cmd.Parameters.AddWithValue("@denda", txbDenda.Text);
+                    cmd.Parameters.AddWithValue("@id_peminjam", cbxIDPeminjam.Text);
+                    cmd.Parameters.AddWithValue("@plat_nmr", cbxPlatNmr.Text);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data Berhasil di Updated");
+                        dataGridView();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message + " (Error Code: " + ex.Number + ")");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+        private void LoadPeminjamData()
+        {
+            try
+            {
+                koneksi.Open();
+
+                string str = "SELECT id_peminjam FROM Peminjam";
+                command = new SqlCommand(str, koneksi);
+                DataTable peminjamTable = new DataTable();
+
+                adapter = new SqlDataAdapter(command);
+                adapter.Fill(peminjamTable);
+
+                cbxIDPeminjam.DisplayMember = "id_peminjam";
+                cbxIDPeminjam.ValueMember = "id_peminjam";
+
+                cbxIDPeminjam.DataSource = peminjamTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                koneksi.Close();
+            }
         }
     }
 }

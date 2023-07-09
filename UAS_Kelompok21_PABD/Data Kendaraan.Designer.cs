08e36efb6a4e56381f70dcd47f97e5b4cd61e92e
Adnurhytia <adindanurhayati30@gmail.com>
2023-07-09 20:59:58 +0700

    Menambahkan CRUD pada form Data Kendaraan

--- UAS_Kelompok21_PABD/Data Kendaraan.Designer.cs
+++ UAS_Kelompok21_PABD/Data Kendaraan.Designer.cs
@@ -41,6 +41,11 @@
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxDelete = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +53,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(232, 27);
+            this.label1.Location = new System.Drawing.Point(359, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(311, 47);
             this.label1.TabIndex = 0;
@@ -116,6 +121,7 @@
             this.cbxJenis.Name = "cbxJenis";
             this.cbxJenis.Size = new System.Drawing.Size(157, 28);
             this.cbxJenis.TabIndex = 7;
+            this.cbxJenis.SelectedIndexChanged += new System.EventHandler(this.cbxJenis_SelectedIndexChanged);
             // 
             // cbxHarga
             // 
@@ -133,7 +139,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(344, 216);
+            this.dataGridView1.Size = new System.Drawing.Size(550, 216);
             this.dataGridView1.TabIndex = 9;
             // 
             // btnAdd
@@ -144,15 +150,17 @@
             this.btnAdd.TabIndex = 10;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(509, 338);
+            this.btnSave.Location = new System.Drawing.Point(509, 325);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(93, 36);
             this.btnSave.TabIndex = 11;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnBack
             // 
@@ -164,12 +172,63 @@
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(254, 324);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(76, 37);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(349, 281);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(76, 37);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(349, 324);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(76, 37);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(122, 375);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "DeletedbyId";
+            // 
+            // tbxDelete
+            // 
+            this.tbxDelete.Location = new System.Drawing.Point(254, 375);
+            this.tbxDelete.Name = "tbxDelete";
+            this.tbxDelete.Size = new System.Drawing.Size(116, 26);
+            this.tbxDelete.TabIndex = 18;
+            // 
             // Data_Kendaraan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(865, 450);
+            this.ClientSize = new System.Drawing.Size(1088, 450);
+            this.Controls.Add(this.tbxDelete);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnAdd);
@@ -207,5 +266,10 @@
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxDelete;
     }
 }
--- UAS_Kelompok21_PABD/Data Kendaraan.cs
+++ UAS_Kelompok21_PABD/Data Kendaraan.cs
@@ -2,19 +2,53 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using System.Windows.Input;
 
 namespace UAS_Kelompok21_PABD
 {
     public partial class Data_Kendaraan : Form
     {
+        private string stringConnection = "Data Source=DESKTOP-4IT269M\\ADINDANURHAYATI;Initial Catalog=Peminjaman_Kendaraan;User ID=sa;Password=3007dinda";
+        private SqlConnection koneksi;
+        private SqlCommand command;
+        private SqlDataAdapter adapter;
+
+        private void refreshform()
+        {
+            cbxIDRental.Text = "";
+            tbxPlatNmr.Text = "";
+            cbxJenis.Text = "";
+            cbxHarga.Text = "";
+            btnSave.Enabled = true;
+            btnUpdate.Enabled = true;
+        }
         public Data_Kendaraan()
         {
             InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            refreshform();
+            dataGridView();
+            LoadRentalData();
+        }
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select * from dbo.Kendaraan";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
         }
 
         private void Data_Kendaraan_Load(object sender, EventArgs e)
@@ -26,6 +60,141 @@ namespace UAS_Kelompok21_PABD
         {
             Form1 myForm1 = new Form1();
             myForm1.Show();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            cbxIDRental.Enabled = true;
+            tbxPlatNmr.Enabled = true;
+            btnSave.Enabled = true;
+            btnUpdate.Enabled = true;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string str = "UPDATE Kendaraan SET plat_nmr= @plat_nmr,jenis_kendaraan= @jenis_kendaraan,harga_sewa= @harga_sewa";
+
+            using (SqlConnection conn = new SqlConnection(stringConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand(str, conn))
+                {
+                    cmd.Parameters.AddWithValue("@plat_nmr", tbxPlatNmr.Text);
+                    cmd.Parameters.AddWithValue("@jenis_kendaraan", cbxJenis.Text);
+                    cmd.Parameters.AddWithValue("@harga_sewa", cbxHarga.Text);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data Berhasil di Updated");
+                        dataGridView();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message + " (Error Code: " + ex.Number + ")");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string str = "DELETE FROM Kendaraan WHERE plat_nmr = @plat_nmr";
+
+            using (SqlConnection conn = new SqlConnection(stringConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand(str, conn))
+                {
+                    cmd.Parameters.AddWithValue("@plat_nmr", tbxDelete.Text);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data Berhasil Dihapus");
+                        dataGridView();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message + " (Error Code: " + ex.Number + ")");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string jnsKendaraan = cbxJenis.Text;
+            string pltNomor = tbxPlatNmr.Text;
+            string idRental = cbxIDRental.Text;
+            string hrgSewa = cbxHarga.Text;
+
+            if (pltNomor == "")
+            {
+                MessageBox.Show("Masukkan Plat Nomor Kendaraan", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "insert into dbo.Kendaraan(id_rental,plat_nmr,jenis_kendaraan,harga_sewa,)" + "values(@id_rental,@plat_nmr,@jenis_kendaraan,@harga_sewa)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("id_rental", idRental));
+                cmd.Parameters.Add(new SqlParameter("plat_nmr", pltNomor));
+                cmd.Parameters.Add(new SqlParameter("jenis_kendaraan", jnsKendaraan));
+                cmd.Parameters.Add(new SqlParameter("harga_sewa", hrgSewa));
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
+            }
+        }
+        private void LoadRentalData()
+        {
+            try
+            {
+                koneksi.Open();
+
+                string str = "SELECT id_rental FROM Rental";
+                command = new SqlCommand(str, koneksi);
+                DataTable rentalTable = new DataTable();
+
+                adapter = new SqlDataAdapter(command);
+                adapter.Fill(rentalTable);
+
+                cbxIDRental.DisplayMember = "id_rental";
+                cbxIDRental.ValueMember = "id_rental";
+
+                cbxIDRental.DataSource = rentalTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                koneksi.Close();
+            }
+        }
+
+        private void cbxJenis_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }

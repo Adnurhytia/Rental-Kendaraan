a42f8c848cdb28bb1b6c8b489ed6958e007a3468
Adnurhytia <adindanurhayati30@gmail.com>
2023-07-10 10:46:06 +0700

    Menambahkan method untuk memilih Id Rental berdasarkan Id peminjam

--- UAS_Kelompok21_PABD/Data Peminjaman.cs
+++ UAS_Kelompok21_PABD/Data Peminjaman.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using System.Windows.Input;
 
 namespace UAS_Kelompok21_PABD
 {
@@ -30,6 +36,11 @@ namespace UAS_Kelompok21_PABD
         {
             Form1 myForm1 = new Form1();
             myForm1.Show();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
